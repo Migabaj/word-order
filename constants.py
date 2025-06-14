55da6c0f79a28b7bac9f7c269fa138a86785f0c7
Mikhail Sonkin <michael.sonkin@gmail.com>
2025-06-14 21:29:09 +0200

    updated constants

--- constants.py
+++ constants.py
@@ -1,11 +1,13 @@
-PWD = '/nethome/msonkin/word_order_logitlens/'
-CACHE_DIR = "/scratch/msonkin/word_order_logitlens/cache/"
+PWD = '/nethome/msonkin/word-order-thesis/'
+CACHE_DIR = "/scratch/msonkin/word-order-thesis/cache/"
 RANDOM_STATE = 22
+
+ATTN_VIS_PATH = "word-order-thesis/fake_attn/"
 
 VERBNOUN_COUNT_TOP3_PATH = "results/verbnoun_count/top3.json"
 VERBNOUN_COUNT_TOP1_PATH = "results/verbnoun_count/top1.json"
-VERBNOUN_CSV_PATH = "data/verbnoun.csv"
-SENTENCE_CSV_PATH = "data/dataset.csv"
+VERBNOUN_CSV_PATH = "word-order-thesis/data/verbnoun.csv"
+SENTENCE_CSV_PATH = "word-order-thesis/data/dataset.csv"
 
 perf_pattern = "{noun_nom} {haben} {article_acc} {noun_acc} {verb_perf}"
 impf_pattern = "{noun_nom} {verb_impf} {article_acc} {noun_acc} {verb_prefix}"
