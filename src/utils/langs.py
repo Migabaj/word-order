b60703326da00ba974600a45365e183b3fdf7368
Mikhail Sonkin <michael.sonkin@gmail.com>
2026-01-11 15:03:54 +0100

    Add language label information

---
+++ src/utils/langs.py
@@ -0,0 +1,28 @@
+# This format does not end with the end quote because the target sentence
+# could be incomplete and the final quote is unimportant for the research.
+ONESHOT_FORMAT = """{lang_src}: \"{sentence_src}\" - {lang_tgt}: \"{sentence_tgt}\""""
+ONESHOT_FORMAT_CUTOFF = """{lang_src}: \"{sentence_src}\" - {lang_tgt}: \""""
+
+iso2language = {
+    "eng": "English",
+    "fre": "Français",
+    "rus": "Русский",
+    "ger": "Deutsch",
+    "spa": "Español",
+    "ita": "Italiano",
+    "zho": "中文",
+    "tur": "Türkçe",
+    "ned": "Nederlands",
+}
+
+iso2language_eng = {
+    "eng": "English",
+    "fre": "French",
+    "rus": "Russian",
+    "ger": "German",
+    "spa": "Spanish",
+    "ita": "Italian",
+    "zho": "Chinese",
+    "tur": "Turkish",
+    "ned": "Dutch",
+}
